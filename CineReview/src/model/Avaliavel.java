e064d6225b1cf437a6fb86a9879715ca80546e72
R-Schmidt-dev <rcs.address.t@gmail.com>
2026-06-19 15:28:24 -0300

    Update Avaliavel.java

--- CineReview/src/model/Avaliavel.java
+++ CineReview/src/model/Avaliavel.java
@@ -2,9 +2,7 @@ package model;
 
 import util.AvaliacaoInvalidaException;
 
-/**
- * Define o contrato para conteudos que podem receber avaliacoes.
- */
+
 public interface Avaliavel {
     void adicionarAvaliacao(Avaliacao avaliacao) throws AvaliacaoInvalidaException;
 
